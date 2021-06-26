8b911412c33b0d2f08d7669ef888f79ca844a874
O. O. Nathan <nateoguns.work@gmail.com>
2021-06-26 08:45:15 +0100

    updates package.json file

--- src/_env.js
+++ src/_env.js
@@ -18,10 +18,10 @@ const config = {
         FB_REDIRECT_URI: ``,
     },
     production: {
-        APP_PORT: 8000,
+        APP_PORT: 4000,
         FB_APP_ID: 159432622819982,
         FB_GRAPH_URI: `https://graph.facebook.com`,
-        FB_REDIRECT_URI: ``,
+        FB_REDIRECT_URI: `https://youthful-morse-692f0a.netlify.app/`,
     },
 }
 
